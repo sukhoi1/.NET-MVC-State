5747213fa32e16e78d89e903af4e6275074d4ca0
Anton Lyhin <Anton_Lyhin@epam.com>
2016-11-04 18:14:50 +0300

    v0.004 - ViewBag example added;

---
+++ MvcState/Controllers/ViewBagController.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Mvc;
+
+namespace MvcState.Controllers
+{
+    public class ViewBagController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ProcessFirstForm(string name)
+        {
+            Debug("Name: {0}", (object)name);
+            return View("SecondForm", (object)name);
+        }
+
+        [HttpPost]
+        public ActionResult CompleteForm(string country)
+        {
+            Debug("Country: {0}", (object)country);
+            ViewBag.Name = "<Unknown>";
+            ViewBag.Country = country;
+            return View();
+        }
+
+        private void Debug(string name, object value)
+        {
+            System.Diagnostics.Debug.WriteLine(name, value);
+        }
+    }
+}

dc25857569d6d4cbe66db8b896a0e0dd3246fa73
Anton Lygin <lygin@live.com>
2016-11-05 18:14:11 +0300

    v0.006 - SessionLifetime example;

---
+++ MvcState/Controllers/SessionLifecycleController.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading;
+using System.Web.Mvc;
+using MvcState.Infrastructure;
+
+namespace MvcState.Controllers
+{
+    public class SessionLifecycleController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(bool store = false, bool abandon = false)
+        {
+            if (store)
+            {
+                SessionStateHelper.Set(SessionStateKey.Name, "Name");
+            }
+
+            if (abandon)
+            {
+                Session.Abandon();
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}

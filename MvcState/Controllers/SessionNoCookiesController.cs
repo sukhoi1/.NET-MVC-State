1fb69abb2b56660c0145466b24fe523e4844deae
Anton Lygin <lygin@live.com>
2016-11-06 16:13:55 +0300

    v0.008 - no session cookies example;

---
+++ MvcState/Controllers/SessionNoCookiesController.cs
@@ -0,0 +1,11 @@
+﻿namespace MvcState.Controllers
+{
+    public class SessionNoCookiesController : SessionLifecycleController
+    {
+        // Note: when we click a link : @Html.ActionLink("Click Me", "Index") it contains the session id.
+        // As result "Is New?" is always true on Submit.
+
+        // But submit form Html.BeginForm() has no session id until the page name is specified explicitly Html.BeginForm("Index").
+        // As result "Is New?" is always false on link press and page refresh.
+    }
+}
